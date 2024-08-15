1a018ae87fd6844138a98e1e48cbb53148459e3c
abhimanyu0x <abhimanyu.verma.ug21@nsut.ac.in>
2024-08-15 15:13:36 +0530

    POTD

---
+++ Q-860.cpp
@@ -0,0 +1,32 @@
+/*
+860. Lemonade Change
+
+At a lemonade stand, each lemonade costs $5. Customers are standing in a queue to buy from you and order one at a time (in the order specified by bills). Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill. You must provide the correct change to each customer so that the net transaction is that the customer pays $5.
+
+Note that you do not have any change in hand at first.
+
+Given an integer array bills where bills[i] is the bill the ith customer pays, return true if you can provide every customer with the correct change, or false otherwise.
+
+*/
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int c5 = 0, c10 = 0;
+        for (auto bill : bills) {
+            if (bill == 5)
+                c5++;
+            else if (bill == 10 && c5 >= 1) {
+                c10++;
+                c5--;
+            } else if (bill == 20 && c10 >= 1 && c5 >= 1) {
+                c10--;
+                c5--;
+            } else if (bill == 20 && c5 >= 3) {
+                c5 -= 3;
+            } else
+                return false;
+        }
+        return true;
+    }
+};
